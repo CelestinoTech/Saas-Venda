64c7253ceea33bb3709138bc3122bf71fb4945da
Gabriel Celestino de Souza <celestinoprofissional74@gmail.com>
2026-05-29 00:29:03 -0300

    Criando cadastro de Estado

---
+++ sistema/src/main/java/com/projeto/sistema/controle/EstadoControle.java
@@ -0,0 +1,30 @@
+package com.projeto.sistema.controle;
+
+import com.projeto.sistema.modelos.Estado;
+import com.projeto.sistema.repositorios.EstadoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class EstadoControle {
+    @Autowired
+    private EstadoRepositorio estadoRepositorio;
+    @GetMapping("/cadastroEstado")
+    public ModelAndView cadastrar(Estado estado){
+        ModelAndView mv = new ModelAndView("administrativo/estados/cadastros");
+        mv.addObject("estado", estado);
+        return mv;
+    }
+    @PostMapping("/salvarEstado")
+    public  ModelAndView salvar(Estado estado, BindingResult result){
+        if (result.hasErrors()){
+            return cadastrar(estado);
+        }
+        estadoRepositorio.saveAndFlush(estado);
+        return cadastrar(new Estado());
+    }
+}
