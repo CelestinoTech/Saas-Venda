6cc939a1ecf4e39ac49343c20cb140c2f9180e34
Gabriel Celestino de Souza <celestinoprofissional74@gmail.com>
2026-05-29 21:28:53 -0300

    Listagem, Remoção e edição de Estado

--- sistema/src/main/java/com/projeto/sistema/controle/EstadoControle.java
+++ sistema/src/main/java/com/projeto/sistema/controle/EstadoControle.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 public class EstadoControle {
@@ -19,6 +22,26 @@ public class EstadoControle {
         mv.addObject("estado", estado);
         return mv;
     }
+    @GetMapping("/editarEstado/{id}")
+    public ModelAndView editar(@PathVariable("id")Long id){
+        Optional<Estado> estado = estadoRepositorio.findById(id);
+        return cadastrar(estado.get());
+    }
+    @GetMapping("/removerEstado/{id}")
+    public ModelAndView remover(@PathVariable("id")Long id){
+        Optional<Estado> estado = estadoRepositorio.findById(id);
+        estadoRepositorio.delete(estado.get());
+        return listar();
+    }
+    @GetMapping("/listarEstado")
+    public ModelAndView listar(){
+        ModelAndView mv = new ModelAndView("/administrativo/estados/lista");
+        mv.addObject("listaEstados",estadoRepositorio.findAll());
+        return mv;
+    }
+
+
+
     @PostMapping("/salvarEstado")
     public  ModelAndView salvar(Estado estado, BindingResult result){
         if (result.hasErrors()){
